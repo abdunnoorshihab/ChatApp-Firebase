11d1881fdf9630ca94292282ac268cd4a66ec848
Abdun Noor Shihab <abdunnoorshihab@gmail.com>
2024-04-27 23:45:46 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat Application - Fahim',
+      title: 'Chat Application - Shihab',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
